88cfd5e30a49a28b7be66c0419ef0579f32f13f0
Sahasra <sahasrakondapalli43@gmail.com>
2026-06-23 18:43:55 -0400

    update day 11, add exam 2 review material

---
+++ Week 6/day11.cpp
@@ -0,0 +1,23 @@
+/*
+Tuesday June 23rd, 2026 - Session 11 - Exam 2 Review
+*/
+
+// Session Attendance Form: https://forms.cloud.microsoft/r/jY8PFHyxPB
+// Only counts if you were in the meeting for 30 minutes
+
+// Compiled version, includes all Peer Leading attendance quiz questions: Will be added after class
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <stdexcept>
+
+using namespace std;
+
+int main() {
+    cout << "You're gonna do great!" << endl; 
+
+    return 0;
+}
